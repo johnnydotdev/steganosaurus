fb58c4e7d6d874df34ba2ef912a72085daa88233
Johnny Chang <johnny@johnnychang.com>
2015-09-24 00:15:01 -0500

    added small CImg test file

---
+++ cimg_test.cpp
@@ -0,0 +1,14 @@
+#include "CImg.h"
+#include <iostream>
+
+using namespace cimg_library;
+using namespace std;
+
+int main() {
+    CImg<unsigned char> image("sloth.jpg");
+    const unsigned char color[] = { 255, 255, 255 };
+    image.draw_point(50, 50, color);
+    image.save("drake.jpg");
+
+    return 0;
+}
